2764f0601fc620fab0185c4a0a4e2560433542b7
Fadhail <mochammadfadhail2@gmail.com>
2026-03-04 01:33:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#152BEB', // The vibrant blue from the image
+                footer: '#45108A',  // Deep purple for footer
+                lightbg: '#F8F9FA', // Light grey/blue
+            },
+            fontFamily: {
+                sans: ['Outfit', 'sans-serif'],
+            }
+        }
+    }
+};
